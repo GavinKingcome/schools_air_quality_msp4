11acd5f6100e85ce7f2a708ac3d288bcfe8192e6
GavinKingcome <gavin@gavinkingcome.com>
2025-12-17 22:04:20 +0000

    Add schools, airquality, and maps Django apps

--- schools_air_quality_msp4/settings.py
+++ schools_air_quality_msp4/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Project apps
+    'schools',
+    'airquality',
+    'maps',
 ]
 
 MIDDLEWARE = [
