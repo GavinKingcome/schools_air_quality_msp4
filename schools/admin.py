a1aae90af67ac10bc2932d2c140cad2af9df8127
GavinKingcome <gavin@gavinkingcome.com>
2025-12-27 22:22:40 +0000

    Register School model in Django admin

--- schools/admin.py
+++ schools/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import School
 
 # Register your models here.
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ['name', 'city', 'postcode', 'school_type', 'student_count']
+    list_filter = ['school_type', 'city']
+    search_fields = ['name', 'address', 'city', 'postcode']
+    ordering = ['name']
+
